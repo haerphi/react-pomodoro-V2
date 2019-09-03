335a0084a93f63a8e5876c2e7281b2586418b45e
Philippe Haerens <haerphi@gmail.com>
2019-09-03 16:57:24 +0200

    ajout de la transformation seconde - minute

--- src/components/time.js
+++ src/components/time.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 function Time(props) {
-    return <h1 className={"timer"}>{`${props.minute}:${props.second}`}</h1>;
+    const minute = Math.floor(props.second / 60);
+    const second = props.second % 60;
+    //format the time here
+    return <h1 className={"timer"}>{`${minute}:${second}`}</h1>;
 }
 
 export default Time;
