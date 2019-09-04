c07362e6a02f7461eb9cc94c083d5f1959c122ca
Philippe Haerens <haerphi@gmail.com>
2019-09-04 09:33:50 +0200

    Update render format
    Ajout du padStart

--- src/components/time.js
+++ src/components/time.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Time(props) {
-    const minute = Math.floor(props.second / 60);
-    const second = props.second % 60;
+    //transformation des secondes en minute:seconde
+    let minute = Math.floor(props.second / 60);
+    let second = props.second % 60;
+    //rajout du 0 s'il y a moins de 2 chiffres
+    minute = `${minute}`.padStart(2, 0);
+    second = `${second}`.padStart(2, 0);
     //format the time here
     return <h1 className={"timer"}>{`${minute}:${second}`}</h1>;
 }
